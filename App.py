3a0775a8cb2c258a7a7602e5696a35b1b00cce66
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-11-19 00:01:34 -0500

    append Invidual for populations

--- App.py
+++ App.py
@@ -15,9 +15,11 @@ class App(object):
         self.minLengthObjetive = 1
         self.maxPopulation = 300
         self.minPopulation = 100
-        self.minRateMutation = 0
-        self.maxRateMutation = 1
+        self.minRateMutation = 0.0
+        self.maxRateMutation = 1.0
+        self.populations = []
         self.inputParams()
+        self.executeGeneticAlgorithm()
 
     def inputParams(self):
         self.inputObjetive()
@@ -34,29 +36,36 @@ class App(object):
             raise Exception("Exception: El objetivo es muy extenso!")
 
     def inputPopulation(self):
-        self.population=int(
+        self.numberPopulation=int(
             input("Ingrese poblacion total de individuos [100 a 300]: "))
-        if (self.population == 0):
+        if (self.numberPopulation == 0):
             raise Exception(
                 "Exception: La poblacion de individuos es invalida!")
-        if (self.population < self.minPopulation):
+        if (self.numberPopulation < self.minPopulation):
             raise Exception(
                 "Exception: La poblacion de individuos es muy reducida!")
-        if (self.population > self.maxPopulation):
+        if (self.numberPopulation > self.maxPopulation):
             raise Exception(
                 "Exception: La poblacion de individuos es muy extensa!")
 
     def inputRateMutation(self):
-        self.population=float(input("Ingrese la tasa de mutacion [0 a 1]: "))
-        if (self.population < self.minRateMutation):
+        self.rateMutation=float(input("Ingrese la tasa de mutacion [0 a 1]: "))
+        if (self.rateMutation < self.minRateMutation):
             raise Exception(
                 "Exception: La tasa de mutacion no puede ser menor de %s" % self.minRateMutation)
-        if (self.population < self.maxRateMutation):
+        if (self.rateMutation > self.maxRateMutation):
             raise Exception(
                 "Exception: La tasa de mutacion no puede ser mayor de %s" % self.maxRateMutation)
+    
+    def executeGeneticAlgorithm(self):
+        for _ in range(1, (self.numberPopulation + 1)):
+            self.populations.append(Individual(len(self.objetive), self.objetive))
+        
+        for j in range(1,5):
+            print("Individuo %s : %s" % (j,self.populations[j].getPhenotype()))
 
 if __name__ == "__main__":
     try:
-        app=App()
+        app = App()
     except (ValueError, FileNotFoundError, AttributeError, Exception) as ex:
         print(ex)
