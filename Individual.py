8c243f5c32ab1ba59d33f8b4e4647aa2f55c1a2b
Gonzalo Chacaltana <gchacaltanab@gmail.com>
2019-11-18 23:24:40 -0500

    initial commit for Individual Class

---
+++ Individual.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Individual Class
+"""
+__author__ = "Gonzalo Chacaltana Buleje"
+__email__ = "gchacaltanab@gmail.com"
+
+class Individual(object):
+
+    def __init__(self):
+        pass
