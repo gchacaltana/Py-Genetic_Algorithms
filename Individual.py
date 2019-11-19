8a2ecdb287096bc9c24c060e55e63709935f78ac
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-11-19 00:01:15 -0500

    add phenotype generation for Individual Class

--- Individual.py
+++ Individual.py
@@ -5,8 +5,19 @@ Individual Class
 """
 __author__ = "Gonzalo Chacaltana Buleje"
 __email__ = "gchacaltanab@gmail.com"
+from random import randint
 
 class Individual(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, numberGenes, objetive):
+        self.numberGenes = numberGenes
+        self.objetive = objetive
+        self.minNumberChar = 32
+        self.maxNumberChar = 128
+        self.genes = []
+        for _ in range(1, (self.numberGenes + 1)):
+            self.genes.append(chr(randint(self.minNumberChar, self.maxNumberChar)))
+    
+    def getPhenotype(self):
+        return ''.join(self.genes)
+
