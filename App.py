3de3ad38b192a0816aca073b6f93b6a54c3c4a13
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2023-06-17 22:53:29 -0500

    update app

--- App.py
+++ App.py
@@ -1,119 +1,158 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-# App Script
+"""
+Aplicación que implementa un algoritmo de optimización utilizando algoritmos genéticos.
+"""
 __author__ = "Gonzalo Chacaltana Buleje"
-__email__ = "gchacaltanab@gmail.com"
 
 import sys
 from random import randint
-from Individual import Individual
+from individual import Individual
 
 
 class App(object):
+    """
+    Clase que implementa la aplicación de consola
+    """
 
     def __init__(self):
-        self.maxLengthObjetive = 300
-        self.minLengthObjetive = 1
-        self.maxPopulation = 300
-        self.minPopulation = 100
-        self.minRateMutation = 0.0
-        self.maxRateMutation = 1.0
+        self.max_length_objetive: int = 300
+        self.min_length_objetive: int = 1
+        self.max_population: int = 300
+        self.min_population: int = 100
+        self.min_rate_mutation: float = 0.0
+        self.max_rate_mutation: float = 1.0
+        self.number_population: int = 0
+        self.objetive: str = ""
         self.populations = []
-        self.inputParams()
-        self.executeGeneticAlgorithm()
+        self.parents = []
 
-    def inputParams(self):
-        self.inputObjetive()
-        self.inputPopulation()
-        self.inputRateMutation()
+    def run(self):
+        """
+        Método principal de la aplicación
+        """
+        self.input_params()
+        self.execute_genetic_algorithm()
 
-    def inputObjetive(self):
+    def input_params(self):
+        """
+        Método para el ingreso de parámetros
+        """
+        self.input_objetive()
+        self.input_population()
+        self.input_rate_mutation()
+
+    def input_objetive(self):
+        """
+        Método para el ingreso del texto objetivo
+        """
         self.objetive = input("Ingrese el texto objetivo: ")
-        if (len(self.objetive) == 0):
+        if len(self.objetive) == 0:
             raise Exception("Exception: El objetivo no fue ingresado!")
-        if (len(self.objetive) < self.minLengthObjetive):
+        if len(self.objetive) < self.min_length_objetive:
             raise Exception("Exception: El objetivo es muy corto!")
-        if (len(self.objetive) > self.maxLengthObjetive):
+        if len(self.objetive) > self.max_length_objetive:
             raise Exception("Exception: El objetivo es muy extenso!")
 
-    def inputPopulation(self):
-        self.numberPopulation = int(
+    def input_population(self):
+        """
+        Método para el ingreso de datos de la población
+        """
+        self.number_population = int(
             input("Ingrese cantidad de individuos por poblacion [100 a 300]: "))
-        if (self.numberPopulation == 0):
+        if (self.number_population == 0):
             raise Exception(
                 "Exception: La poblacion de individuos es invalida!")
-        if (self.numberPopulation < self.minPopulation):
+        if (self.number_population < self.min_population):
             raise Exception(
                 "Exception: La poblacion de individuos es muy reducida!")
-        if (self.numberPopulation > self.maxPopulation):
+        if (self.number_population > self.max_population):
             raise Exception(
                 "Exception: La poblacion de individuos es muy extensa!")
 
-    def inputRateMutation(self):
-        self.rateMutation = float(
+    def input_rate_mutation(self):
+        """
+        Método para el ingreso del ratio de mutación.
+        """
+        self.rate_mutation = float(
             input("Ingrese la tasa de mutacion [0 a 1]: "))
-        if (self.rateMutation < self.minRateMutation):
+        if (self.rate_mutation < self.min_rate_mutation):
             raise Exception(
-                "Exception: La tasa de mutacion no puede ser menor de %s" % self.minRateMutation)
-        if (self.rateMutation > self.maxRateMutation):
+                "Exception: La tasa de mutacion no puede ser menor de %s" % self.min_rate_mutation)
+        if (self.rate_mutation > self.max_rate_mutation):
             raise Exception(
-                "Exception: La tasa de mutacion no puede ser mayor de %s" % self.maxRateMutation)
+                "Exception: La tasa de mutacion no puede ser mayor de %s" % self.max_rate_mutation)
 
-    def executeGeneticAlgorithm(self):
-        for _ in range(0, self.numberPopulation):
+    def execute_genetic_algorithm(self):
+        """
+        Método para ejecutar el algoritmo genético
+        """
+        for _ in range(0, self.number_population):
             individual = Individual()
-            individual.generateGenes(len(self.objetive), self.objetive)
+            individual.generate_genes(len(self.objetive), self.objetive)
             self.populations.append(individual)
         self.generation = 0
         print("Buscando mejor individuo....")
         while True:
-            self.evaluateMembersGeneration()
-            self.selectMembersGeneration()
-            self.reproductionMembersGeneration()
+            self.evaluate_members_generation()
+            self.select_members_generation()
+            self.reproduction_members_generation()
 
-    def evaluateMembersGeneration(self):
+    def evaluate_members_generation(self):
+        """
+        Método para evaluar miembros de una generación
+        """
         self.generation += 1
-        print("\n*************** GENERACION %s\n" % self.generation)
-        for _ in range(0, self.numberPopulation):
-            print("Generacion[%s] | Individuo[%s]: %s | fitness: %s" % (
-                self.generation, _, self.populations[_].getPhenotype(), self.populations[_].getFitness()))
-            if (self.evaluateObjetive(self.populations[_])):
+        print(f"\n GENERACION {self.generation}\n")
+        for x in range(0, self.number_population):
+            print(
+                f"Generacion[{self.generation}] | Individuo[{x}]: {self.populations[x].get_phenotype()} | fitness: {self.populations[x].get_fitness()}")
+            if self.evaluate_objetive(self.populations[x]):
                 sys.exit()
 
-    def selectMembersGeneration(self):
+    def select_members_generation(self):
+        """
+        Método para seleccionar a los miembros de una generación
+        """
         self.parents = []
-        for _ in range(0, self.numberPopulation):
-            n = int(self.populations[_].getFitness()*100)
-            #for j in range(0, n):
-            if n>0:
+        for _ in range(0, self.number_population):
+            n = int(self.populations[_].get_fitness()*100)
+            if n > 0:
                 self.parents.append(self.populations[_])
 
-    def reproductionMembersGeneration(self):
-        totalParents = len(self.parents)
-        print("Padres seleccionados: ", totalParents)
-        for i in range(0, self.numberPopulation):
-            a = int(randint(0, (totalParents-1)))
-            b = int(randint(0, (totalParents-1)))
+    def reproduction_members_generation(self):
+        """
+        Método que implementa la reproducción de miembros de una generación
+        """
+        total_parents: int = len(self.parents)
+        print("Padres seleccionados: ", total_parents)
+        for i in range(0, self.number_population):
+            a = int(randint(0, (total_parents-1)))
+            b = int(randint(0, (total_parents-1)))
             father = self.parents[a]
             mother = self.parents[b]
             children = father.cross(mother)
-            children.mutate(self.rateMutation)
+            children.mutate(self.rate_mutation)
             self.populations[i] = children
 
-    def evaluateObjetive(self, individual):
-        if (individual.getFitness() == 1.0):
-            print("Objetivo encontrado: %s" % individual.getPhenotype())
+    def evaluate_objetive(self, individual: Individual):
+        """
+        Método para evaluar objetivo
+        """
+        if individual.get_fitness() == 1.0:
+            print(f"Objetivo encontrado: {individual.get_phenotype()}")
             return True
         return False
 
-    def showIndividualPhenotype(self):
-        for j in range(0, self.numberPopulation):
-            print("Individuo %s : %s" %
-                  (j, self.populations[j].getPhenotype()))
+    def show_individual_phenotype(self):
+        """
+        Método para mnostrar fenotipo individual
+        """
+        for j in range(0, self.number_population):
+            print(f"Individuo {j} : {self.populations[j].get_phenotype()}")
 
 
 if __name__ == "__main__":
     try:
         app = App()
+        app.run()
     except (ValueError, FileNotFoundError, AttributeError, Exception) as ex:
         print(ex)
